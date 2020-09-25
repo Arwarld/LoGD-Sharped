ae293d7e304b19fac506629ef2256db8a0c80ab0
Marius Knabben <marius.knabben@rwth-aachen.de>
2020-09-25 15:13:30 +0200

    Changed build behaviour

--- LoGD-Core/Game/Database.cs
+++ LoGD-Core/Game/Database.cs
@@ -1,7 +1,14 @@
-﻿namespace LoGD.Core.Game
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace LoGD.Core.Game
 {
     public class Database
     {
-
+        private SqlConnection connection;
+        public Database()
+        {
+            SqlConnection connection = new SqlConnection();
+        }
     }
 }
--- LoGD-Server/GameMaster.cs
+++ LoGD-Server/GameMaster.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 
 namespace LoGD.Server
 {
